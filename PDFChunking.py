c544d374d9c1612b2da8007f7d79c863e9b3b0c0
lemuruz <nakabodeepatiparn@gmail.com>
2026-02-08 21:24:25 +0700

    add pdf chunking , ollama Embedding models and store it in chromadb

---
+++ PDFChunking.py
@@ -0,0 +1,41 @@
+import pypdf,ollama,chromadb
+
+class readPDF:
+    def __init__(self,PDFfile):
+        self.PDFfile = PDFfile
+        self.PDFtext = ""
+        reader = pypdf.PdfReader(PDFfile)
+        # for i in range(0,len(reader.pages)):
+        #     self.PDFtext += reader.pages[i].extract_text()
+
+        self.PDFtext = "".join([p.extract_text() for p in reader.pages])
+        self.textlen = len(self.PDFtext)
+        self.PDFchunked = []
+        self.collection = None
+
+    
+    def PDFChunking(self,chunkSize=700,overlapSize=20):
+        step = chunkSize-overlapSize
+        if step <= 0:raise ValueError("Chunk size must be greater than overlap size.")
+        for i in range(0,self.textlen,step):
+            self.PDFchunked.append(self.PDFtext[i:i+chunkSize])
+        
+        
+
+    def PDFEmbeddedAndStore(self,db_path, collection_name):
+        client = chromadb.PersistentClient(path=db_path)
+        
+        collection = client.get_or_create_collection(name=collection_name)
+        if collection.count() == 0:
+            print(f"Embedding new file: {collection_name}")
+            for i, d in enumerate(self.PDFchunked):
+                response = ollama.embed(model="mxbai-embed-large", input=d)
+                embeddings = response["embeddings"]
+                collection.add(
+                    ids=[str(i)],
+                    embeddings=embeddings,
+                    documents=[d]
+                )
+        else:
+            print(f"Using existing embeddings for: {collection_name}")
+        self.collection = collection
--- message.py
+++ message.py
@@ -1,7 +1,28 @@
-import ollama
+import ollama,PDFChunking,os,chromadb
 
-client = ollama.Client()
+FILE_PATH = "cookbook.pdf"
+Database_Directory = "./chroma_db"
+collectionName = FILE_PATH.split('.')[0]
 
+
+
+
+pdf = PDFChunking.readPDF(FILE_PATH)
+pdf.PDFChunking(chunkSize=700, overlapSize=20)
+pdf.PDFEmbeddedAndStore(db_path=Database_Directory,collection_name=collectionName)
+
+
+user_input = "What should I cook today?"
+
+response = ollama.embed(
+  model="mxbai-embed-large",
+  input=user_input
+)
+results = pdf.collection.query(
+  query_embeddings=[response["embeddings"][0]],
+  n_results=2
+)
+data = results['documents'][0][0]
 message = [
     {
         'role':'system',
@@ -9,10 +30,11 @@ message = [
     },
     {
         'role': 'user',
-        'content': 'What should I cook today?',
+        'content': f"Question: {user_input} \n\n Context from my book: {data}",
     },
 ]
 
+client = ollama.Client()
 for responsePart in client.chat('llama3',messages=message,
     stream=True):
     print(responsePart.message.content,end='',flush=True)
