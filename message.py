1e60f670428a58159b4f26ed798dd66ce7b18755
lemuruz <nakabodeepatiparn@gmail.com>
2026-02-10 20:25:57 +0700

    addd loop so the program dont exit after one ans use role assistant to let the ai have 'memory'

--- message.py
+++ message.py
@@ -12,29 +12,44 @@ pdf.PDFChunking(chunkSize=700, overlapSize=20)
 pdf.PDFEmbeddedAndStore(db_path=Database_Directory,collection_name=collectionName)
 
 
-user_input = "What should I cook today?"
 
-response = ollama.embed(
-  model="mxbai-embed-large",
-  input=user_input
-)
-results = pdf.collection.query(
-  query_embeddings=[response["embeddings"][0]],
-  n_results=2
-)
-data = results['documents'][0][0]
-message = [
-    {
-        'role':'system',
-        'content':'You are a professional chef.',
-    },
-    {
-        'role': 'user',
-        'content': f"Question: {user_input} \n\n Context from my book: {data}",
-    },
+print("Chef Bot is ready! Type 'exit' to stop.")
+# user_input = "What should I cook today?"
+conversation_history = [
+    {'role': 'system', 'content': 'You are a professional chef.'}
 ]
+while True:
+    user_input = input("input : ")
+    if user_input == "exit":
+        break
+    response = ollama.embed(
+        model="mxbai-embed-large",
+        input=user_input
+    )
+    results = pdf.collection.query(
+        query_embeddings=[response["embeddings"][0]],
+        n_results=2
+    )
+    data = results['documents'][0][0]
+    conversation_history.append(
+        {
+            'role': 'user',
+            'content': f"Question: {user_input} \n\n Context from my book: {data}",
+        },
+    )
 
-client = ollama.Client()
-for responsePart in client.chat('llama3',messages=message,
-    stream=True):
-    print(responsePart.message.content,end='',flush=True)
+    client = ollama.Client()
+    fullResponse = ""
+    for responsePart in client.chat('llama3',messages=conversation_history,
+        stream=True):
+        text = responsePart.message.content
+        print(text,end='',flush=True)
+        fullResponse += responsePart.message.content
+    print("\n")
+    conversation_history.append(
+        {
+            'role': 'assistant',
+            'content': fullResponse
+        }
+    )
+
