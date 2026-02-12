062c91a3fb8a003ef1bcc91a52138bdf5b38c611
lemuruz <nakabodeepatiparn@gmail.com>
2026-02-12 19:17:16 +0700

    use PDFChunking for chunking content,use chat_history to vector_store.similarity_search so the model know the topic and know what's 'it' mean

--- PDFChunking.py
+++ PDFChunking.py
@@ -1,14 +1,16 @@
-import pypdf,ollama,chromadb
+import pypdf,pymupdf 
 
 class readPDF:
     def __init__(self,PDFfile):
         self.PDFfile = PDFfile
         self.PDFtext = ""
-        reader = pypdf.PdfReader(PDFfile)
+        # reader = pypdf.PdfReader(PDFfile)
+        reader = pymupdf.open(PDFfile)
         # for i in range(0,len(reader.pages)):
         #     self.PDFtext += reader.pages[i].extract_text()
-
-        self.PDFtext = "".join([p.extract_text() for p in reader.pages])
+        for page in reader:
+            self.PDFtext += page.get_text()
+        # self.PDFtext = "".join([p.extract_text() for p in reader.pages])
         self.textlen = len(self.PDFtext)
         self.PDFchunked = []
         self.collection = None
@@ -22,20 +24,21 @@ class readPDF:
         
         
 
-    def PDFEmbeddedAndStore(self,db_path, collection_name):
-        client = chromadb.PersistentClient(path=db_path)
+    # def PDFEmbeddedAndStore(self,db_path, collection_name):
+    #     client = chromadb.PersistentClient(path=db_path)
         
-        collection = client.get_or_create_collection(name=collection_name)
-        if collection.count() == 0:
-            print(f"Embedding new file: {collection_name}")
-            for i, d in enumerate(self.PDFchunked):
-                response = ollama.embed(model="mxbai-embed-large", input=d)
-                embeddings = response["embeddings"]
-                collection.add(
-                    ids=[str(i)],
-                    embeddings=embeddings,
-                    documents=[d]
-                )
-        else:
-            print(f"Using existing embeddings for: {collection_name}")
-        self.collection = collection
+    #     collection = client.get_or_create_collection(name=collection_name)
+    #     if collection.count() == 0:
+    #         print(f"Embedding new file: {collection_name}")
+    #         for i, d in enumerate(self.PDFchunked):
+    #             response = ollama.embed(model="mxbai-embed-large", input=d)
+    #             embeddings = response["embeddings"]
+    #             collection.add(
+    #                 ids=[str(i)],
+    #                 embeddings=embeddings,
+    #                 documents=[d]
+    #             )
+    #     else:
+    #         print(f"Using existing embeddings for: {collection_name}")
+    #     self.collection = collection
+
--- message.py
+++ message.py
@@ -5,7 +5,7 @@ from langchain_ollama import OllamaEmbeddings
 from langchain_classic.chains.retrieval import create_retrieval_chain
 from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
-
+from langchain_core.documents import Document
 llm = ChatOllama(model="llama3")
 embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 
@@ -14,84 +14,70 @@ FILE_PATH = "cookbook.pdf"
 Database_Directory = "./chroma_db"
 collectionName = FILE_PATH.split('.')[0]
 
+pdf_reader = PDFChunking.readPDF(FILE_PATH)
+pdf_reader.PDFChunking(chunkSize=700, overlapSize=100) 
+
+documents = [Document(page_content=chunk) for chunk in pdf_reader.PDFchunked]
 
 vector_store = Chroma(
     persist_directory=Database_Directory, 
     embedding_function=embeddings,
     collection_name=collectionName
 )
+if vector_store._collection.count() == 0:
+    print("Embedding new documents...")
+    vector_store.add_documents(documents)
+else:
+    print("Using existing embeddings...")
 
-retriever = vector_store.as_retriever()
-prompt = ChatPromptTemplate.from_template("""
-You are a professional chef. Answer the user's question based ONLY on the context below.
+# retriever = vector_store.as_retriever()
+# prompt = ChatPromptTemplate.from_template("""
+# You are a professional chef. Answer the user's question based ONLY on the context below.
                                           
-<context>
-{context}
-</context>
+# <context>
+# {context}
+# </context>
 
-Question: {input}
-""")
-document_chain = create_stuff_documents_chain(llm, prompt)
-retrieval_chain = create_retrieval_chain(retriever, document_chain)
+# Question: {input}
+# """)
+# document_chain = create_stuff_documents_chain(llm, prompt)
+# retrieval_chain = create_retrieval_chain(retriever, document_chain)
 print("LangChain Chef is ready!")
 
 # test
 # test_docs = retriever.invoke("Stuffed Potatoes")
 # print(f"Found {len(test_docs)} chunks!")
 # print(f"First chunk content: {test_docs[0].page_content[:100]}...")
-
+full_prompt = """You are a professional chef. Answer the user's question based ONLY on the context below.                                        
+    <context>
+    {context}
+    </context>
+    Chat History:{chat_history}
+    Question: {user_input}
+    """
+chat_history = []
 while True:
     user_input = input("You: ")
     if user_input.lower() == "exit": break
+
     
+
+    if chat_history:
+        # last_question = chat_history[-1][0]
+        search_query = f"{chat_history} {user_input}"
+    else:
+        search_query = user_input
+    relevant_docs = vector_store.similarity_search(search_query, k=6)    
+    context = "\n\n".join([doc.page_content for doc in relevant_docs])
+    
+
     # This one line does: Embedding -> Searching -> Context Stuffing -> Generating
-    response = retrieval_chain.invoke({"input": user_input})
-    
-    print("Chef:", response['answer'])
-
-# pdf = PDFChunking.readPDF(FILE_PATH)
-# pdf.PDFChunking(chunkSize=700, overlapSize=20)
-# pdf.PDFEmbeddedAndStore(db_path=Database_Directory,collection_name=collectionName)
-
+    # response = retrieval_chain.invoke({"input": user_input})
+    # Generate answer (what llm does)
+    response = llm.invoke(full_prompt.format(context=context, user_input=user_input,chat_history=chat_history))
+    chat_history.append((user_input, response.content))
+    print("Chef:", response.content)
+    # print("Chef:", response['answer'])
 
 
-# print("Chef Bot is ready! Type 'exit' to stop.")
-# # user_input = "What should I cook today?"
-# conversation_history = [
-#     {'role': 'system', 'content': 'You are a professional chef.'}
-# ]
-# while True:
-#     user_input = input("input : ")
-#     if user_input == "exit" or "quit":
-#         break
-#     response = ollama.embed(
-#         model="mxbai-embed-large",
-#         input=user_input
-#     )
-#     results = pdf.collection.query(
-#         query_embeddings=[response["embeddings"][0]],
-#         n_results=2
-#     )
-#     data = results['documents'][0][0]
-#     conversation_history.append(
-#         {
-#             'role': 'user',
-#             'content': f"Question: {user_input} \n\n Context from my book: {data}",
-#         },
-#     )
-
-#     client = ollama.Client()
-#     fullResponse = ""
-#     for responsePart in client.chat('llama3',messages=conversation_history,
-#         stream=True):
-#         text = responsePart.message.content
-#         print(text,end='',flush=True)
-#         fullResponse += responsePart.message.content
-#     print("\n")
-#     conversation_history.append(
-#         {
-#             'role': 'assistant',
-#             'content': fullResponse
-#         }
-#     )
 
