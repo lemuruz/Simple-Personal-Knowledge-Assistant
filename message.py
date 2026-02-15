3b4f768a42856a116bf5473d05a0ba25455220d7
lemuruz <nakabodeepatiparn@gmail.com>
2026-02-15 15:40:36 +0700

    make the model reply in strict format

--- message.py
+++ message.py
@@ -1,3 +1,4 @@
+from urllib import response
 import PDFChunking
 from langchain_ollama import ChatOllama
 from langchain_chroma import Chroma
@@ -6,11 +7,19 @@ from langchain_community.chat_message_histories import FileChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
 from langchain_core.documents import Document
 from langchain_core.messages import SystemMessage
+from pydantic import BaseModel, Field
 
 
+class RecipeOutput(BaseModel):
+    message: str = Field(..., description="The generated response from the AI Chef.")
+    ingredients: list[str] = Field(..., description="List of ingredients needed for the recipe.")
+    instructions: list[str] = Field(..., description="Step-by-step instructions for preparing the dish.") 
+    tips_message: str = Field(..., description="Fun tips related to the recipe or cooking in general.")
+    
 class AI:
     def __init__(self, file_path, db_directory,role = ""):
-        self.llm = ChatOllama(model="llama3")
+
+        self.llm = ChatOllama(model="llama3", temperature=0).with_structured_output(RecipeOutput)
         self.embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 
         self.file_path = file_path
@@ -35,20 +44,30 @@ class AI:
             print("Using existing embeddings...")
 
         self.memory = None
-        self.prompt = ChatPromptTemplate(
-            input_variables=['content'],
-            messages=[
-                SystemMessage(content=f'You are a professional {role}.'),
-                MessagesPlaceholder(variable_name='chat_history'),
-                HumanMessagePromptTemplate.from_template(
-                    "Context:\n{context}\n\nQuestion:\n{user_input}"
-                )
-            ]
-        )
+        self.prompt = ChatPromptTemplate.from_messages([
+            SystemMessage(
+                content=f"""You are a professional {role}.
+                Respond in a friendly, human conversational tone.
+
+                Also return structured recipe data.
+
+                The "message" field should contain a warm, natural explanation or recommendation.
+                The "tips_message" field should explain the tips in a fun,friendly, natural paragraph style.
+                
+                The other fields must contain clean structured data.
+                """
+                ),
+
+            MessagesPlaceholder(variable_name='chat_history'),
+            HumanMessagePromptTemplate.from_template(
+                "Context:\n{context}\n\nQuestion:\n{user_input}"
+            )
+        ])
         print("LangChain Chef is ready!")
     
     def generate_response(self, user_input):
-        chat_history = self.memory.messages if self.memory else None
+        chat_history = self.memory.messages if self.memory else []
+
         if chat_history:
             history_text = " ".join([m.content for m in chat_history])
             search_query = f"{history_text} {user_input}"
@@ -58,89 +77,16 @@ class AI:
         context = "\n\n".join([doc.page_content for doc in relevant_docs])
         
         
-        chain = self.prompt | self.llm
+        chain = self.prompt | self.llm  
         response = chain.invoke({
             "user_input": user_input,
             "context": context,
             "chat_history": chat_history
         })
         self.memory.add_user_message(user_input)
-        self.memory.add_ai_message(response.content)
-        return response.content
+        self.memory.add_ai_message(str(response))
 
-
-
-
-# llm = ChatOllama(model="llama3")
-# embeddings = OllamaEmbeddings(model="mxbai-embed-large")
-
-
-# FILE_PATH = "cookbook.pdf"
-# Database_Directory = "./chroma_db"
-# collectionName = FILE_PATH.split('.')[0]
-# history = FileChatMessageHistory('.chat_history.json')
-
-# pdf_reader = PDFChunking.readPDF(FILE_PATH)
-# pdf_reader.PDFChunking(chunkSize=700, overlapSize=100) 
-
-# documents = [Document(page_content=chunk) for chunk in pdf_reader.PDFchunked]
-
-# vector_store = Chroma(
-#     persist_directory=Database_Directory, 
-#     embedding_function=embeddings,
-#     collection_name=collectionName
-# )
-# if vector_store._collection.count() == 0:
-#     print("Embedding new documents...")
-#     vector_store.add_documents(documents)
-# else:
-#     print("Using existing embeddings...")
-
-# memory: ConversationBufferMemory = ConversationBufferMemory(
-#     memory_key='chat_history',
-#     chat_memory=history,
-#     return_messages=True
-# )
-
-# print("LangChain Chef is ready!")
-
-# prompt = ChatPromptTemplate(
-#     input_variables=['content'],
-#     messages=[
-#         SystemMessage(content='You are a professional chef.'),
-#         MessagesPlaceholder(variable_name='chat_history'),
-#         HumanMessagePromptTemplate.from_template(
-#             "Context:\n{context}\n\nQuestion:\n{user_input}"
-#         )
-#     ]
-# )
-
-
-
-# while True:
-#     user_input = input("You: ")
-#     if user_input.lower() == "exit": break
-
-    
-#     chat_history = memory.load_memory_variables({})['chat_history']
-#     if chat_history:
-#         # last_question = chat_history[-1][0]
-#         search_query = f"{chat_history} {user_input}"
-#     else:
-#         search_query = user_input
-#     relevant_docs = vector_store.similarity_search(search_query, k=6)    
-#     context = "\n\n".join([doc.page_content for doc in relevant_docs])
-    
-
-#     chain = prompt | llm
-#     response = chain.invoke({
-#         "user_input": user_input,
-#         "context": context,
-#         "chat_history": chat_history
-#     })
-#     memory.save_context({"content": user_input}, {"content": response.content})
-#     print("Chef:", response.content)
-#     # print("Chef:", response['answer'])
+        return response.model_dump()
 
 
 
